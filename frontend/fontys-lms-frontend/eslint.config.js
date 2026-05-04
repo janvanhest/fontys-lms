e71fda4cb5c5d602ecd643efdc4dec45d52a0a6d
Jan van Hest <janvanhest@outlook.com>
2026-05-04 23:36:21 +0200

    feat: update ESLint configuration to include additional React plugins and rules

--- frontend/fontys-lms-frontend/eslint.config.js
+++ frontend/fontys-lms-frontend/eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -14,12 +16,19 @@ export default defineConfig([
       tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
         projectService: true,
       },
+    },
+    rules: {
+      'max-lines': ['warn', { max: 150, skipBlankLines: true, skipComments: true }],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 ])
