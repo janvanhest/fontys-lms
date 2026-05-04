2a2966876472fc089afb46e1014c1aefd7dc26c4
Jan van Hest <janvanhest@outlook.com>
2026-05-05 00:12:44 +0200

    chore: setup Storybook with Vite builder and MUI theme
    - Initialiseert Storybook 10 met @storybook/react-vite
    - Configureert preview.tsx met ThemeProvider en Roboto fonts
      zodat alle stories het Fontys-thema gebruiken
    - Voegt storybook-static en logs toe aan .gitignore

---
+++ frontend/fontys-lms-frontend/.storybook/preview.tsx
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react-vite'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import theme from '../src/theme'
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: { test: 'todo' },
+  },
+}
+
+export default preview
