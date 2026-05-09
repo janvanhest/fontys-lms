7309eafcb11427f31f476a475a1d20324696c26d
Jan van Hest <janvanhest@outlook.com>
2026-05-09 16:46:57 +0200

    feat: update getInfo method to return dynamic package name and version

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import packageJson from '../package.json';
 
 @Injectable()
 export class AppService {
   getInfo(): { name: string; version: string } {
-    return { name: 'Fontys LMS API', version: '1.0.0' };
+    return {
+      name: packageJson.name,
+      version: packageJson.version,
+    };
   }
 }
