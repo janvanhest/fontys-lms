3dc8d2f9c04de9fd0d446b8dc0cef7a41a189870
Jan van Hest <janvanhest@outlook.com>
2026-05-18 22:07:41 +0200

    feat(student): add Student entity

---
+++ backend/src/student/student.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  canvasUserId: string;
+
+  @Column()
+  displayName: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true, type: 'varchar' })
+  avatarUrl: string | null;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
